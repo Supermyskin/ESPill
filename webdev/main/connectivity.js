8bfea568d48b6b0823192a61887bafc4471bc4c2
Supermyskin <supermyskin@abv.bg>
2026-03-26 13:56:12 +0200

    Added actual connectivity for the ESP

--- webdev/main/connectivity.js
+++ webdev/main/connectivity.js
@@ -1,31 +1,23 @@
-const brokerUrl = 'wss://c1d253884ce6464f85c084b6f8d77bbc.s1.eu.hivemq.cloud:8884/mqtt';
-
-const options = {
-    username: 'HackerCatcho',
-    password: 'YxjyiPT!t_v5T_9'
-};
-
-const client = mqtt.connect(brokerUrl, options);
+const client = mqtt.connect('wss://507f68c94c1b48c6b9a345e8a073e5cd.s1.eu.hivemq.cloud:8884/mqtt', {
+    username: 'ESPill',
+    password: '1Qazxsw23edcvfr4'
+});
 
 client.on('connect', () => {
-    document.getElementById('status').innerText = "Connected! Ready for data.";
-    document.getElementById('status').style.color = "green";
+    client.subscribe('esp32/has_taken_pill');
 
-    console.log("Subscribed to esp32/subscribed");
+    document.getElementById('status').innerText = "Connected! Ready for data.";
+    document.getElementById('status').style.color = "rgb(253, 117, 255)";
+    console.log("subscribed to esp32");
 });
 
 client.on('message', (topic, message) => {
-    if (topic === 'esp32/water_sensor') {
+    if (topic === 'esp32/has_taken_pill') {
         try {
             const data = JSON.parse(message.toString());
-            console.log(data.water_level)
+            console.log(data.pill_taken)
         } catch (e) {
-            console.log("Could not read JSON data", message.toString());
+            console.log("JSON data error: ", message.toString());
         }
     }
 });
-
-function requestWaterData() {
-    client.publish('esp32/receive', 'GET_WATER');
-    console.log("Command 'GET_WATER' sent to esp32/receive");
-}

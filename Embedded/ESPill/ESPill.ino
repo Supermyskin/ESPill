b7478324a64616ad491ccce4f80deacf181c6f5b
yurzzilla <bu277188@gmail.com>
2026-03-26 15:07:32 +0200

    Clock Module Connected

--- Embedded/ESPill/ESPill.ino
+++ Embedded/ESPill/ESPill.ino
@@ -1,17 +1,25 @@
 #include "HeaderFile.h"
+#include "Servo.h"
+#include "RTClib.h"
 
+
+
+Servo Servo1;
+char buf[20];
+DS1302 rtc(CLOCK_RST, CLOCK_CLK, CLOCK_DATA);
 void setup(){
   Serial.begin(9600);
   pinMode(ECHO, INPUT);
   pinMode(TRIG, OUTPUT);
+  pinMode(BUZZ, OUTPUT);
+  Servo1.attach(8);
+  if(!rtc.isrunning()){
+    rtc.begin();
+  }
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 }
 
 
-
-
 void loop(){
-  Serial.println("Distance: ");
-  Serial.print(CheckDistance());
-
-  delay(100);
+  delay(1000);
 }

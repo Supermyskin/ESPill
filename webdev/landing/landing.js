c46863aab77932ecc969c68623b32f7917df44ea
Supermyskin <supermyskin@abv.bg>
2026-03-26 14:34:44 +0200

    Added cursor underglow and formatted the css

--- webdev/landing/landing.js
+++ webdev/landing/landing.js
@@ -51,3 +51,11 @@ window.onload = function () {
         }
     }
 };
+
+document.addEventListener("mousemove", (event) => {
+    let mousex = event.clientX - 15;
+    let mousey = event.clientY - 15;
+    let curglow = document.querySelector('.cursor-glow');
+    curglow.style.left = mousex / 16 + 'rem';
+    curglow.style.top = mousey / 16 + 'rem';
+});
